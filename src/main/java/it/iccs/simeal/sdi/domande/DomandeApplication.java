178c7da6557cd5f15305aff3bfb2758386326858
Danilo Pazzelli <danilo.pazzelli@iccs.it>
2022-03-24 15:23:42 +0100

    first commit

---
+++ src/main/java/it/iccs/simeal/sdi/domande/DomandeApplication.java
@@ -0,0 +1,13 @@
+package it.iccs.simeal.sdi.domande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomandeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomandeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/iccs/simeal/sdi/domande/SdiDomandeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.iccs.simeal.sdi.domande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdiDomandeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
